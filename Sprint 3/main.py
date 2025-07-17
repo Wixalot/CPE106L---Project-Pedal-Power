905295c54c3668e3a388ac29b16d4b004b64395a
ALONZO, Cyan Miles <alonzocyan@gmail.com>
2025-07-17 21:07:19 +0800

    polished main program

--- Sprint 3/main.py
+++ Sprint 3/main.py
@@ -2,11 +2,5 @@ import sys
 
 # UIs Import
 from UIs.LoginWINDOW import execute, LoginWindow
-from UIs.UserInterface import User_Interface_Window
-
-# Database Import
-from Database.table_db import *
-
-from PyQt5.QtWidgets import QApplication
 
 exec = execute()
