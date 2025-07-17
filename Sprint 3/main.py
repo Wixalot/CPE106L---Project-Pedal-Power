e4c5854958d17524d55e0023910259715834b6ea
ALONZO, Cyan Miles <alonzocyan@gmail.com>
2025-07-17 21:04:59 +0800

    uploaded main program

---
+++ Sprint 3/main.py
@@ -0,0 +1,9 @@
+from LoginWINDOW import LoginWindow
+import sys
+import os
+
+# Importing the LoginWindow class from the UIs module
+ui_path = os.path.join(os.path.dirname(__file__), "Sprint 3", "UIs")
+sys.path.append(ui_path)
+
+# importing the LoginWindow class
